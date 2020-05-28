2186102e91d65a9e26eea6408fb67a2815c89331
vivien <vivien.boucher@outlook.com>
2020-05-28 11:25:33 +0200

    before

--- src/SommeArgentTest.java
+++ src/SommeArgentTest.java
@@ -1,35 +1,49 @@
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 class SommeArgentTest {
-    SommeArgent m12CHF= new SommeArgent(12, "CHF");
-    SommeArgent m14CHF= new SommeArgent(14, "CHF");
-    SommeArgent m14USD= new SommeArgent(14, "USD");
-    SommeArgent expected = new SommeArgent(26, "CHF");
+    SommeArgent m14USD;
+    SommeArgent m14CHF;
+    SommeArgent m12CHF;
+
+
+    @BeforeEach
+    public void initparam() {
+        m14USD= new SommeArgent(14, "USD");
+        m12CHF= new SommeArgent(12, "CHF");
+        m14CHF= new SommeArgent(14, "CHF");
+    }
+
 
     @Test
     void getQuantite() {
-
+        Assertions.assertEquals(14, m14USD.getQuantite());
     }
 
     @Test
     void getUnite() {
+        Assertions.assertTrue( m12CHF.getUnite().equals(m12CHF.getUnite()));
     }
 
     @Test
     void add() {
-        SommeArgent m12CHF= new SommeArgent(12, "CHF");
-        SommeArgent m14CHF= new SommeArgent(14, "CHF");
         SommeArgent result = m12CHF.add(m14CHF);
         Assertions.assertEquals(26, result.getQuantite());
     }
 
     @Test
     void testEquals() {
-        SommeArgent m12CHF= new SommeArgent(12, "CHF");
-        SommeArgent m14CHF= new SommeArgent(14, "CHF");
-        SommeArgent result = m12CHF.add(m14CHF);
-        Assertions.assertTrue(result.equals(result));
+        // Verify that m12CHF != null; throw True
+        Assertions.assertTrue(!m12CHF.equals(null));
+        // Verify that m12CHF = m12CHF; should throw True
+        Assertions.assertEquals(m12CHF, m12CHF);
+        // Verify that m12CHF != new SommeArgent; throw False
+        Assertions.assertNotEquals(m12CHF, new SommeArgent (12, "CHF"));
+        // Verify that m12CHF != m14CHF ? throw True
+        Assertions.assertTrue(!m12CHF.equals(m14CHF));
+        // Verify that m14USD != m14CHF ? throw True
+        Assertions.assertTrue(!m14USD.equals(m14CHF));
     }
 }
